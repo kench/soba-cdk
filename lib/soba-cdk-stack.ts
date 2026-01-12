0651bd018d27f7720c8ee2faa934b1efee4acc46
Kenley Cheung <ken@lordkenthegreat.com>
2026-01-11 16:11:15 -0800

    Add services website

--- lib/soba-cdk-stack.ts
+++ lib/soba-cdk-stack.ts
@@ -1,6 +1,12 @@
 import * as cdk from 'aws-cdk-lib/core';
-import { HostedZone } from 'aws-cdk-lib/aws-route53';
+import { AaaaRecord, ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
 import { Construct } from 'constructs';
+import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3BucketOrigin, S3StaticWebsiteOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Certificate, CertificateValidation, DnsValidatedCertificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SobaCdkStack extends cdk.Stack {
@@ -19,5 +25,44 @@ export class SobaCdkStack extends cdk.Stack {
       zoneName: 'services.seattleoba.org',
     });
     hostedZone.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+
+    // S3 bucket for website
+    const websiteBucket = new Bucket(this, 'ServicesWebsiteBucket', {
+      accessControl: BucketAccessControl.PRIVATE,
+    });
+
+    // Deploy static website assets into bucket
+    const websiteBucketDeployment = new BucketDeployment(this, 'ServicesWebsiteBucketDeployment', {
+      sources: [Source.asset('./assets/services-website/build/client')],
+      destinationBucket: websiteBucket
+    });
+
+    // Certificate for CloudFront distribution
+    // DnsValidatedCertificate is used since we need the certificate to be created in us-east-1 for CloudFront to use it,
+    // despite the construct's deprecation.
+    const certificate = new DnsValidatedCertificate(this, 'ServicesWebsiteCertificate', {
+      domainName: 'services.seattleoba.org',
+      hostedZone: hostedZone,
+      region: 'us-east-1'
+    });
+
+    // Create CloudFront disctribution
+    const servicesWebsiteDistribution = new Distribution(this, 'ServicesWebsiteCloudFrontDistribution', {
+      certificate: certificate,
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessControl(websiteBucket)
+      },
+      domainNames: ['services.seattleoba.org']
+    });
+
+    new AaaaRecord(this, 'ServicesWebsiteAAAARecord', {
+      zone: hostedZone,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(servicesWebsiteDistribution))
+    });
+    new ARecord(this, 'ServicesWebsiteARecord', {
+      zone: hostedZone,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(servicesWebsiteDistribution))
+    });
   }
 }
