191c2db427b70ca88efc6d207cfbe11af3d98894
Kenley Cheung <ken@lordkenthegreat.com>
2026-01-13 18:51:45 -0800

    Set secret

--- lib/secrets.ts
+++ lib/secrets.ts
@@ -3,11 +3,14 @@ import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
 
 class SecretsStack extends NestedStack {
+    public twitchClientSecret: Secret;
+
     constructor(scope: Construct, id: string) {
         super(scope, id);
         const twitchClientSecret = new Secret(this, 'TwitchClientSecret', {
             secretName: 'soba-twitch-client-secret'
         })
+        this.twitchClientSecret = twitchClientSecret;
     }
 }
 
